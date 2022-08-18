9596c05f8543fe4cd6190245b003001757e00aba
Devin Leamy <devinleamy@gmail.com>
2022-08-18 14:13:39 -0600

    added color picking

--- client/src/element_display.tsx
+++ client/src/element_display.tsx
@@ -2,6 +2,7 @@ import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import { Button, FormControl, Card, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, Slider } from "@mui/material";
 import React, { ReactNode, useEffect, useRef, useState } from "react"; 
 import { RequestEmitter, WorkerPool } from "./wasm_loader";
+import { SliderPicker, CompactPicker, CirclePicker } from "react-color";
 
 import "./styles.css";
 
@@ -14,26 +15,16 @@ const METAL = "Metal"
 const LAMBERTIAN = "Lambertian"
 const DIELECTRIC = "Dielectric"
 
-const TEXUTURE_DEFAULT = { SolidTexture: { color: { red: 0.2, green: 0.2, blue: 0.9 } } };
+const TEXTURE_DEFAULT = { SolidTexture: { color: { red: 0.2, green: 0.2, blue: 0.9 } } };
+const SOLID_TEXTURE_DEFAULT = structuredClone(TEXTURE_DEFAULT);
+const CHECKERED_TEXTURE_DEFAULT = { CheckeredTexture: {
+    odd: { red: 1.0, green: 1.0, blue: 1.0 }, 
+    even: { red: 0.2, green: 0.2, blue: 0.2 }, 
+}}
 
-const LAMBERTIAN_DEFAULT = {
-    Lambertian: {
-        texture: TEXUTURE_DEFAULT
-    }
-}
-
-const METAL_DEFAULT = {
-    Metal: {
-        fuzz: 0.0,
-        texture: TEXUTURE_DEFAULT
-    }
-}
-
-const DIELECTRIC_DEFAULT = {
-    Dielectric: {
-        ref_index: 1.5
-    }
-}
+const LAMBERTIAN_DEFAULT = { Lambertian: { texture: TEXTURE_DEFAULT } }
+const METAL_DEFAULT = { Metal: { fuzz: 0.0, texture: TEXTURE_DEFAULT } }
+const DIELECTRIC_DEFAULT = { Dielectric: { ref_index: 1.5 } }
 
 let threadCount = 5; // navigator.hardwareConcurrency;
 
@@ -60,7 +51,7 @@ const ElementDisplay = ({ element, onElementUpdate }: ElementDisplayProps) => {
         setPool(pool);
     }, [])
 
-    console.log("Displayed element", element)
+    // console.log("Displayed element", element)
 
     const renderElement = (element) => {
         requestEmitter.render_element(element, pool)
@@ -152,7 +143,25 @@ const ElementDisplay = ({ element, onElementUpdate }: ElementDisplayProps) => {
 const MaterialDisplay = ({ material, onMatChange }) => {
     let matType = objName(material);
 
-    console.log("Mat", material)
+    // console.log("Mat", material)
+
+    const onTextureChange = (newTexture) => {
+        console.log("Updated texture", newTexture)
+        let materialClone = structuredClone(material);
+
+        switch (matType) {
+            case METAL: {
+                materialClone.Metal.texture = newTexture;
+                onMatChange(materialClone);
+                break;
+            }
+            case LAMBERTIAN: {
+                materialClone.Lambertian.texture = newTexture;
+                onMatChange(materialClone);
+                break;
+            }
+        } 
+    }
 
     const displayOptions = () => {
         switch (matType) {
@@ -161,7 +170,7 @@ const MaterialDisplay = ({ material, onMatChange }) => {
             case DIELECTRIC: return displayDielectric(material.Dielectric);
         }
 
-        return <>Error</> 
+        return <>Invalid material</> 
     }
     
     
@@ -188,12 +197,15 @@ const MaterialDisplay = ({ material, onMatChange }) => {
                     valueLabelDisplay="auto" 
                     onChange={updateFuzz}
                 />
+                <TextureDisplay texture={texture} onTextureChange={onTextureChange} />
             </div>
         )
     }
     
-    const displayLambertian = (m) : ReactJSXElement => {
-        return <>Lambertian</> 
+    const displayLambertian = ({ texture }) : ReactJSXElement => {
+        return (
+            <TextureDisplay texture={texture} onTextureChange={onTextureChange} />
+        )
     }
 
     const displayDielectric = ({ ref_index }) : ReactJSXElement => {
@@ -213,7 +225,6 @@ const MaterialDisplay = ({ material, onMatChange }) => {
             { value: 2.4, label: 'Diamond', },
         ];
 
-        // TODO: Add 
         return (
             <div>
                 <div className="config-slider-label">{`Refractive Index (${ref_index})`}</div>
@@ -247,10 +258,8 @@ const MaterialDisplay = ({ material, onMatChange }) => {
     const onMatTypeChange = (_e, newMatType) => {
         if (newMatType === matType) return;
 
-        console.log()
-
         switch (newMatType) {
-            case METAL: { onMatChange(METAL_DEFAULT); break; }
+            case METAL:      { onMatChange(METAL_DEFAULT);      break; }
             case LAMBERTIAN: { onMatChange(LAMBERTIAN_DEFAULT); break; }
             case DIELECTRIC: { onMatChange(DIELECTRIC_DEFAULT); break; }
         }
@@ -278,5 +287,148 @@ const MaterialDisplay = ({ material, onMatChange }) => {
         </div>
     );
 }
+
+const SOLID_TEXTURE = "SolidTexture";
+const CHECKERED_TEXTURE = "CheckeredTexture";
+
+const TextureDisplay = ({ texture, onTextureChange }) => {
+    let textureType = objName(texture);
+
+    // console.log("Texture", texture);
+
+    const displayOptions = () : ReactJSXElement => {
+        switch (textureType) {
+            case SOLID_TEXTURE: return displaySolid(texture.SolidTexture);
+            case CHECKERED_TEXTURE: return displayCheckered(texture.CheckeredTexture);
+        }
+
+        return <>Invalid texture</>
+    }
+
+    const TextureChoice = ({ textureName }) : ReactJSXElement => {
+        let checked = textureName === textureType;
+
+        return (
+            <FormControlLabel 
+                value={textureName}
+                control={<Radio />}
+                label={textureName}
+                checked={checked}
+            />
+        )
+    }
+
+    const displaySolid = ({ color }) : ReactJSXElement => {
+        const onColorChange = (newColor) => {
+            let textureClone = structuredClone(texture); 
+            textureClone.SolidTexture.color = newColor;
+            onTextureChange(textureClone); 
+        }
+
+        return (
+            <div className="color-picker-container">
+                <ColorPicker 
+                    color={color}
+                    onColorChange={onColorChange} 
+                />
+            </div>
+        )
+    }
+
+    const displayCheckered = ({ even, odd }) : ReactJSXElement => {
+        const updateEvenColor = (newColor) => {
+            let textureClone = structuredClone(texture); 
+            textureClone.CheckeredTexture.even = newColor;
+            onTextureChange(textureClone);
+        }
+
+        const updateOddColor = (newColor) => {
+            let textureClone = structuredClone(texture); 
+            textureClone.CheckeredTexture.odd = newColor;
+            onTextureChange(textureClone);
+        }
+
+        // console.log(even, odd)
+
+        return (
+            <div style={{display: "flex"}}>
+                <div style={{marginRight: "10px" }}>
+                    {" "}Even
+                    <ColorPicker 
+                        color={even}
+                        onColorChange={updateEvenColor} 
+                    />
+                </div>
+                <div>
+                    {" "}Odd
+                    <ColorPicker 
+                        color={odd}
+                        onColorChange={updateOddColor}
+                    />
+                </div>
+           </div>
+        )
+    }
+
+    const onTextureTypeChange = (_e, newTextureType: string) => {
+        if (newTextureType === textureType) return;
+
+        switch (newTextureType) {
+            case SOLID_TEXTURE:     { onTextureChange(SOLID_TEXTURE_DEFAULT);     break; }
+            case CHECKERED_TEXTURE: { onTextureChange(CHECKERED_TEXTURE_DEFAULT); break; }
+        }
+    }
+
+    return (
+        <div className="texture-display-container">
+            <div className="texture-types-container">
+                <FormLabel id="texture-button-group">Texture</FormLabel>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    value={textureType}
+                    name="material-button-group"
+                    onChange={onTextureTypeChange}
+                >
+                    <TextureChoice textureName={SOLID_TEXTURE} />
+                    <TextureChoice textureName={CHECKERED_TEXTURE} />
+                </RadioGroup>
+            </div>
+            <div className="texture-options-container">
+                {displayOptions()}
+            </div>
+        </div>
+    )
+}
+
+const ColorPicker = ({ color, onColorChange }) => {
+    const toPickerRGB = (rustRGB) => {
+        return {
+            r: rustRGB.red * 255,
+            g: rustRGB.green * 255,
+            b: rustRGB.blue * 255,
+        }
+    }
+
+    const fromPickerRGB = (pickerRGB) => {
+        return {
+            red: pickerRGB.rgb.r / 255,
+            green: pickerRGB.rgb.g / 255,
+            blue: pickerRGB.rgb.b / 255,
+        }
+   }
+
+    return (
+        <div className="color-picker-container">
+            <CompactPicker
+                color={toPickerRGB(color)} 
+                onChangeComplete={(newColor, _event) => {
+                    onColorChange(fromPickerRGB(newColor));   
+                }}
+            />
+        </div>
+    )
+}
+
 
 export { ElementDisplay }
